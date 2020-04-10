519a334ef92d52297cb64db6740050d98e2c529c
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-04-10 11:32:23 +0200

    Add message formatting

---
+++ lib/vnu/formatter.ex
@@ -0,0 +1,136 @@
+defmodule Vnu.Formatter do
+  @moduledoc false
+
+  alias Vnu.Message
+
+  import IO.ANSI, only: [red: 0, yellow: 0, blue: 0, cyan: 0, reset: 0, reverse: 0]
+
+  @doc false
+  @spec format_messages(list(Message.t())) :: list(String.t())
+  def format_messages(messages) do
+    messages
+    |> sort()
+    |> Enum.map(fn message ->
+      type = type(message)
+      color = color(message)
+
+      location = format_location(message)
+      extract = format_extract(message)
+
+      header = "#{with_color("#{reverse()} #{type} ", color)} #{message.message}"
+
+      extract =
+        case extract do
+          [extract, hilite] ->
+            "#{if location, do: with_color("[#{location}] ", label_color())}#{extract}\n" <>
+              "#{if location, do: String.duplicate(" ", String.length("[#{location}] "))}#{hilite}"
+
+          [extract] ->
+            "#{if location, do: with_color("[#{location}] ", label_color())}#{extract}"
+
+          nil ->
+            nil
+        end
+
+      [header, extract]
+      |> Enum.filter(& &1)
+      |> Enum.join("\n")
+    end)
+  end
+
+  @doc false
+  def sort(messages) do
+    Enum.sort(messages, fn a, b ->
+      al = a.first_line || 0
+      bl = b.first_line || 0
+
+      ac = a.first_column || 0
+      bc = b.first_column || 0
+
+      if al == bl do
+        ac <= bc
+      else
+        al <= bl
+      end
+    end)
+  end
+
+  @doc false
+  def with_color(string, color) do
+    "#{color}#{string}#{reset()}"
+  end
+
+  @doc false
+  def info_color() do
+    blue()
+  end
+
+  @doc false
+  def warning_color() do
+    yellow()
+  end
+
+  @doc false
+  def error_color() do
+    red()
+  end
+
+  @doc false
+  def label_color() do
+    cyan()
+  end
+
+  defp type(%Message{} = message) do
+    case message do
+      %Message{type: :error} -> "Error"
+      %Message{type: :info, sub_type: :warning} -> "Warning"
+      %Message{type: :info, sub_type: nil} -> "Info"
+    end
+  end
+
+  defp color(%Message{} = message) do
+    case message do
+      %Message{type: :error} -> error_color()
+      %Message{type: :info, sub_type: :warning} -> warning_color()
+      %Message{type: :info, sub_type: nil} -> info_color()
+    end
+  end
+
+  defp format_location(%Message{} = message) do
+    case message do
+      %Message{first_line: nil, last_line: nil, first_column: nil, last_column: nil} ->
+        nil
+
+      %Message{first_line: l, last_line: l, first_column: nil, last_column: nil} ->
+        "L#{l}"
+
+      %Message{first_line: l1, last_line: l2, first_column: nil, last_column: nil} ->
+        "L#{l1}-#{l2}"
+
+      %Message{first_line: l, last_line: l, first_column: col1, last_column: col2} ->
+        "L#{l}:#{col1}-#{col2}"
+
+      %Message{first_line: l1, last_line: l2, first_column: col1, last_column: col2} ->
+        "L#{l1}:#{col1}-L#{l2}:#{col2}"
+    end
+  end
+
+  defp format_extract(%Message{} = message) do
+    extract =
+      if message.extract do
+        String.replace(message.extract, "\n", "↩")
+      end
+
+    hilite =
+      if extract && message.hilite_start && message.hilite_length do
+        String.duplicate(" ", message.hilite_start) <>
+          label_color() <>
+          String.duplicate("^", message.hilite_length) <>
+          reset()
+      end
+
+    if extract do
+      if hilite, do: [extract, hilite], else: [extract]
+    end
+  end
+end
---
+++ test/vnu/formatter_test.exs
@@ -0,0 +1,187 @@
+defmodule Vnu.FormatterTest do
+  use ExUnit.Case
+  alias Vnu.{Formatter, Message}
+  import IO.ANSI, only: [red: 0, yellow: 0, blue: 0, cyan: 0, reset: 0, reverse: 0]
+
+  describe "format_messages" do
+    test "doesn't fail when no messages" do
+      assert Formatter.format_messages([]) == []
+    end
+
+    test "uses red for errors, yellow for warnings, and blue for other info" do
+      result =
+        Formatter.format_messages([
+          %Message{
+            type: :error,
+            message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          },
+          %Message{
+            type: :info,
+            sub_type: :warning,
+            message: "Ut venenatis imperdiet magna in dignissim."
+          },
+          %Message{
+            type: :info,
+            message: "Proin ultrices orci vel lectus blandit, ac vulputate lorem placerat."
+          }
+        ])
+
+      assert Enum.count(result) == 3
+
+      assert Enum.at(result, 0) ==
+               "#{red()}#{reverse()} Error #{reset()} Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+      assert Enum.at(result, 1) ==
+               "#{yellow()}#{reverse()} Warning #{reset()} Ut venenatis imperdiet magna in dignissim."
+
+      assert Enum.at(result, 2) ==
+               "#{blue()}#{reverse()} Info #{reset()} Proin ultrices orci vel lectus blandit, ac vulputate lorem placerat."
+    end
+
+    test "sorts by first line, formats location" do
+      messages = [
+        %Message{
+          type: :error,
+          message: "Lorem ipsum 6.",
+          first_line: 6,
+          last_line: 7,
+          extract: "..."
+        },
+        %Message{
+          type: :error,
+          message: "Lorem ipsum 3.",
+          first_line: 1,
+          last_line: 1,
+          extract: "..."
+        },
+        %Message{
+          type: :error,
+          message: "Lorem ipsum 1.",
+          first_line: nil,
+          extract: "..."
+        },
+        %Message{
+          type: :error,
+          message: "Lorem ipsum 2.",
+          first_line: nil,
+          extract: "..."
+        },
+        %Message{
+          type: :error,
+          message: "Lorem ipsum 5.",
+          first_line: 2,
+          last_line: 4,
+          first_column: 1,
+          last_column: 10,
+          extract: "..."
+        },
+        %Message{
+          type: :error,
+          message: "Lorem ipsum 4.",
+          first_line: 1,
+          last_line: 1,
+          first_column: 3,
+          last_column: 6,
+          extract: "..."
+        }
+      ]
+
+      result = Formatter.format_messages(messages)
+
+      assert Enum.count(result) == 6
+
+      assert Enum.at(result, 0) ==
+               "#{red()}#{reverse()} Error #{reset()} Lorem ipsum 1.\n" <>
+                 "..."
+
+      assert Enum.at(result, 1) ==
+               "#{red()}#{reverse()} Error #{reset()} Lorem ipsum 2.\n" <>
+                 "..."
+
+      assert Enum.at(result, 2) ==
+               "#{red()}#{reverse()} Error #{reset()} Lorem ipsum 3.\n" <>
+                 "#{cyan()}[L1] #{reset()}..."
+
+      assert Enum.at(result, 3) ==
+               "#{red()}#{reverse()} Error #{reset()} Lorem ipsum 4.\n" <>
+                 "#{cyan()}[L1:3-6] #{reset()}..."
+
+      assert Enum.at(result, 4) ==
+               "#{red()}#{reverse()} Error #{reset()} Lorem ipsum 5.\n" <>
+                 "#{cyan()}[L2:1-L4:10] #{reset()}..."
+
+      assert Enum.at(result, 5) ==
+               "#{red()}#{reverse()} Error #{reset()} Lorem ipsum 6.\n" <>
+                 "#{cyan()}[L6-7] #{reset()}..."
+    end
+
+    test "hilights the extract" do
+      result =
+        Formatter.format_messages([
+          %Message{
+            type: :info,
+            sub_type: :warning,
+            message: "Lorem ipsum.",
+            extract: "Ut venenatis imperdiet magna in dignissim.",
+            hilite_start: 0,
+            hilite_length: 2
+          },
+          %Message{
+            type: :info,
+            sub_type: :warning,
+            message: "Lorem ipsum.",
+            extract: "Ut venenatis imperdiet magna in dignissim.",
+            hilite_start: 13,
+            hilite_length: 9
+          },
+          %Message{
+            type: :info,
+            sub_type: :warning,
+            message: "Lorem ipsum.",
+            extract: "Ut venenatis imperdiet magna in dignissim.",
+            hilite_start: 13,
+            hilite_length: 9,
+            first_line: 3,
+            last_line: 4,
+            first_column: 34,
+            last_column: 10
+          }
+        ])
+
+      assert Enum.at(result, 0) ==
+               "#{yellow()}#{reverse()} Warning #{reset()} Lorem ipsum.\n" <>
+                 "Ut venenatis imperdiet magna in dignissim.\n" <>
+                 "#{cyan()}^^#{reset()}"
+
+      assert Enum.at(result, 1) ==
+               "#{yellow()}#{reverse()} Warning #{reset()} Lorem ipsum.\n" <>
+                 "Ut venenatis imperdiet magna in dignissim.\n" <>
+                 "             #{cyan()}^^^^^^^^^#{reset()}"
+
+      assert Enum.at(result, 2) ==
+               "#{yellow()}#{reverse()} Warning #{reset()} Lorem ipsum.\n" <>
+                 "#{cyan()}[L3:34-L4:10] #{reset()}Ut venenatis imperdiet magna in dignissim.\n" <>
+                 "                           #{cyan()}^^^^^^^^^#{reset()}"
+    end
+
+    test "replaces newlines in extract with a fancy symbol" do
+      result =
+        Formatter.format_messages([
+          %Message{
+            type: :info,
+            sub_type: :warning,
+            message: "Lorem ipsum.",
+            extract: "Ut\nvenenatis\nimperdiet magna in dignissim.",
+            hilite_start: 13,
+            hilite_length: 9
+          },
+        ])
+
+      assert Enum.at(result, 0) ==
+               "#{yellow()}#{reverse()} Warning #{reset()} Lorem ipsum.\n" <>
+               "Ut↩venenatis↩imperdiet magna in dignissim.\n" <>
+               "             #{cyan()}^^^^^^^^^#{reset()}"
+
+    end
+  end
+end
