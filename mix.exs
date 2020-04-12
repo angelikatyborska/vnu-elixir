eaea0841cefd6417e952f7906ea3920e10befdc3
Angelika Tyborska <angelikatyborska@fastmail.com>
2020-04-12 16:25:13 +0200

    Fix submitting results from CircleCI

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Vnu.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.circle": :test
       ]
     ]
   end
