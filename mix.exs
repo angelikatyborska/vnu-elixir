bd80f605a939649417c2b5c6072e14c41c47dc46
Angelika Cathor <angelikacathor@fastmail.com>
2026-06-27 12:41:21 +0200

    Test with Elixir 1.20 and OTP 29

--- mix.exs
+++ mix.exs
@@ -18,7 +18,12 @@ defmodule Vnu.MixProject do
       name: "Vnu",
       source_url: "https://github.com/angelikatyborska/vnu-elixir/",
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs:  [
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
